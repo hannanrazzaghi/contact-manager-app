4ec2daedeb8bf88682df641a055c56c0f3ef5059
hannanrazzaghi <hannanrazzaghi@gmail.com>
2023-03-24 14:01:27 +0430

    adding main font to project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-    <h3>Managing Contacts application</h3>
+    <h3>Managing Contacts application سلام درود</h3>
     </div>
   );
 }
